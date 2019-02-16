1bf5c0ce8ea9e359e48212d5cd326f74c260da75
Andreas Kater <andreas.kater@gmail.com>
2019-02-16 14:00:40 +0000

    added trace dependencies

--- macros.js
+++ macros.js
@@ -38,3 +38,47 @@ function clear_formatting() {
   spreadsheet.getActiveRangeList().setBorder(false, false, false, false, false, false);
 };
 
+function onOpen() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var menuEntries = []
+  menuEntries.push({name: "Trace Dependents", functionName: "traceDependents"});
+  ss.addMenu("Detective", menuEntries);
+}
+
+function traceDependents(){
+  var dependents = []
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var currentCell = ss.getActiveCell();
+  var currentCellRef = currentCell.getA1Notation();
+  var range = ss.getDataRange();
+
+  var regex = new RegExp("\\b" + currentCellRef + "\\b");
+  var formulas = range.getFormulas();
+
+  for (var i = 0; i < formulas.length; i++){
+    var row = formulas[i];
+
+    for (var j = 0; j < row.length; j++){
+      var cellFormula = row[j].replace(/\$/g, "");
+      if (regex.test(cellFormula)){
+        dependents.push([i,j]);
+      }
+    }
+  }
+
+  var dependentRefs = [];
+  for (var k = 0; k < dependents.length; k ++){
+    var rowNum = dependents[k][0] + 1;
+    var colNum = dependents[k][1] + 1;
+    var cell = range.getCell(rowNum, colNum);
+    var cellRef = cell.getA1Notation();
+    dependentRefs.push(cellRef);
+  }
+  var output = "Dependents: ";
+  if(dependentRefs.length > 0){
+    output += dependentRefs.join(", ");
+  } else {
+    output += " None";
+  }
+  currentCell.setNote(output);
+}

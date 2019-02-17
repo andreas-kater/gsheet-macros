0d238ee3988dcd8379e3262977c83e67b0155399
Andreas Kater <andreas.kater@gmail.com>
2019-02-17 22:32:25 +0100

    trace dependents now goes to first dependent

--- macros.js
+++ macros.js
@@ -46,41 +46,30 @@ function onOpen() {
 }
 
 function trace_dependents(){
-  var dependents = []
   var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var currentCell = ss.getActiveCell();
-  var currentCellRef = currentCell.getA1Notation();
-  var range = ss.getDataRange();
+  var sheets = ss.getSheets();
+  var regex = new RegExp("\\b" + ss.getActiveCell().getA1Notation() + "\\b");
 
-  var regex = new RegExp("\\b" + currentCellRef + "\\b");
-  var formulas = range.getFormulas();
+  for (var s = 0; s < sheets.length ; s++ ) {
+    var sheet = sheets[s];
+    var range = sheet.getDataRange();
+    var formulas = range.getFormulas();
 
-  for (var i = 0; i < formulas.length; i++){
-    var row = formulas[i];
+    for (var i = 0; i < formulas.length; i++){
+      var row = formulas[i];
 
-    for (var j = 0; j < row.length; j++){
-      var cellFormula = row[j].replace(/\$/g, "");
-      if (regex.test(cellFormula)){
-        dependents.push([i,j]);
+      for (var j = 0; j < row.length; j++){
+        var cellFormula = row[j].replace(/\$/g, "");
+        if (regex.test(cellFormula)){
+          var cell = range.getCell(i+1, j+1);
+          var cellRef = cell.getA1Notation();
+          sheet.getRange(cellRef).activate();
+          return 
+        }
       }
     }
   }
-
-  var dependentRefs = [];
-  for (var k = 0; k < dependents.length; k ++){
-    var rowNum = dependents[k][0] + 1;
-    var colNum = dependents[k][1] + 1;
-    var cell = range.getCell(rowNum, colNum);
-    var cellRef = cell.getA1Notation();
-    dependentRefs.push(cellRef);
-  }
-  var output = "Dependents: ";
-  if(dependentRefs.length > 0){
-    output += dependentRefs.join(", ");
-  } else {
-    output += " None";
-  }
-  currentCell.setNote(output);
+  SpreadsheetApp.getUi().alert('No dependencies found');
 }
 
 function trace_precedents(){

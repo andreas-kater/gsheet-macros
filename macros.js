43953c3e843946d7c3220e532e8748f912005b6f
Andreas Kater <andreas.kater@gmail.com>
2019-05-28 10:47:26 +0100

    added space after currency symbols

--- macros.js
+++ macros.js
@@ -6,17 +6,17 @@ function number() {
 
 function gbp() {
   var spreadsheet = SpreadsheetApp.getActive();
-  spreadsheet.getActiveRangeList().setNumberFormat('[$£]#,##0')
+  spreadsheet.getActiveRangeList().setNumberFormat('[$£] #,##0')
 };
 
 function usd() {
   var spreadsheet = SpreadsheetApp.getActive();
-  spreadsheet.getActiveRangeList().setNumberFormat('"$"#,##0');
+  spreadsheet.getActiveRangeList().setNumberFormat('"$ "#,##0');
 };
 
 function eur() {
   var spreadsheet = SpreadsheetApp.getActive();
-  spreadsheet.getActiveRangeList().setNumberFormat('[$€]#,##0')
+  spreadsheet.getActiveRangeList().setNumberFormat('[$€] #,##0')
 };
 
 function datapoint() {
@@ -47,21 +47,21 @@ function clear_formatting() {
 function onOpen() {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var menuEntries = []
-  menuEntries.push({name: "Trace Dependents", functionName: "traceDependents"});
+  menuEntries.push({ name: "Trace Dependents", functionName: "traceDependents" });
   ss.addMenu("Detective", menuEntries);
 }
 
-function trace_precedents(){
+function trace_precedents() {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var formula = ss.getActiveCell().getFormula();
   var regex = /[=(&]*('\w* *\w*'![A-Z]{1,3}[0-9]{1,4})|(\w* *\w*![A-Z]{1,3}[0-9]{1,4})|([A-Z]{1,3}[0-9]{1,4})[=)&]*/g;
   Logger.log(formula);
-  if(formula.charAt(0) == "="){
-    formula = formula.replace("$","");
+  if (formula.charAt(0) == "=") {
+    formula = formula.replace("$", "");
     var matches = regex.exec(formula)
-    if(matches.length > 0){
+    if (matches.length > 0) {
       var goto = matches[1];
-      if (!goto){
+      if (!goto) {
         goto = matches[0];
       }
       ss.getRange(goto).activate();
@@ -70,23 +70,23 @@ function trace_precedents(){
   }
 }
 
-function trace_dependents(){
+function trace_dependents() {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sheets = ss.getSheets();
   var regex = new RegExp("\\b" + ss.getActiveCell().getA1Notation() + "\\b");
 
-  for (var s = 0; s < sheets.length ; s++ ) {
+  for (var s = 0; s < sheets.length; s++) {
     var sheet = sheets[s];
     var range = sheet.getDataRange();
     var formulas = range.getFormulas();
 
-    for (var i = 0; i < formulas.length; i++){
+    for (var i = 0; i < formulas.length; i++) {
       var row = formulas[i];
 
-      for (var j = 0; j < row.length; j++){
+      for (var j = 0; j < row.length; j++) {
         var cellFormula = row[j].replace(/\$/g, "");
-        if (regex.test(cellFormula)){
-          var cell = range.getCell(i+1, j+1);
+        if (regex.test(cellFormula)) {
+          var cell = range.getCell(i + 1, j + 1);
           var cellRef = cell.getA1Notation();
           sheet.getRange(cellRef).activate();
           return
